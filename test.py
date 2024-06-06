7f46a188a57d8c747c1c302aa2e6eca79c36bb56
Ameer Hussein <ameer73@hotmail.co.uk>
2024-06-06 22:36:14 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-GhassanSays =int(input())
+GhassanSays =int(input("I am a parrot, write a number: "))
 
 print (GhassanSays)
+
+numbers = int(input("Write a number: "))
+added = 5
+
+total = print (numbers + added )
